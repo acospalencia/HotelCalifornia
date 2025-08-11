0eeaba869af7b0ec171cbe2263294ca0a2cc1550
Acosta <osoloco1245@gmail.com>
2025-08-11 17:00:24 -0600

    Accion de agregar roles terminada

--- src/main/java/org/esfe/controladores/RoleController.java
+++ src/main/java/org/esfe/controladores/RoleController.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,14 +21,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("/roles")
+@RequestMapping("/role")
 public class RoleController {
 
     @Autowired
     private IRolesService rolesService;
 
     @GetMapping
-    public String Index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
+    public String Index(Role role, Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
         int currentPage = page.orElse(1) - 1 ;
         int pageSize = size.orElse(5);
         Pageable pageable = PageRequest.of(currentPage, pageSize);
@@ -43,5 +46,23 @@ public class RoleController {
         }
 
         return  "role/Index";
+    }
+/*
+    @GetMapping("/create")
+    public String Create(Role role){
+        return "role/create";
+    }
+*/
+
+    @PostMapping("/save")
+    public String save(Role role, BindingResult result, Model model, RedirectAttributes attributes){
+        if (result.hasErrors()) {
+            model.addAttribute(role);
+            attributes.addFlashAttribute("error", "No se pudo guardar debido a un error.");
+            return "role/create";
+        }
+        rolesService.createOrEditOne(role);
+        attributes.addFlashAttribute("msg", "Grupo creado correctamente.");
+        return "redirect:/role";
     }
 }
