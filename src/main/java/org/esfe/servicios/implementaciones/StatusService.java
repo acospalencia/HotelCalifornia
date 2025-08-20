9879e6942d6e016b38628fbacbd97c68efa69eb1
Katherine200423 <katherinequijada6@gmail.com>
2025-08-20 15:41:21 -0600

    Controladores para tipo estado

--- src/main/java/org/esfe/servicios/implementaciones/StatusService.java
+++ src/main/java/org/esfe/servicios/implementaciones/StatusService.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class StatusService implements IStatusService {
+public abstract class StatusService implements IStatusService {
 
     @Autowired
     private IStatusRepository statusRepository;
